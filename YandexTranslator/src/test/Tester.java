b191bfe03d72919388bf02a3ba336689430e199b
Ciunas <ciunas.bennett@gmail.com>
2016-10-26 23:16:30 +0100

    adding back up translation APIs

---
+++ YandexTranslator/src/test/Tester.java
@@ -0,0 +1,14 @@
+//package test;
+//
+//import language.Language;
+//import translate.Translate;
+//
+//public class Tester {
+//    public static void main(String[] args) throws Exception {
+//        Translate.setKey("trnsl.1.1.20161026T181919Z.34f9e508e2fe05e4.113fdcd58c662f93fd97685c53b4fe9f674222c6");
+//
+//        String translatedText = Translate.execute("Hola, mundo!", Language.SPANISH, Language.ENGLISH);
+//
+//        System.out.println(translatedText);
+//    }
+//}
