9a450257e92135b4167b6e363ec9f24b383f3bc2
Ciunas <ciunas.bennett@gmail.com>
2017-02-27 18:23:52 +0000

    threads for websocket connections started

--- transalteIMS/src/ims_client/IMS_Connect.java
+++ transalteIMS/src/ims_client/IMS_Connect.java
@@ -11,8 +11,18 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JScrollPane; 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.event.ActionListener;
+import javax.swing.JTextPane;
 
 public class IMS_Connect extends JDialog  {
 	
@@ -20,13 +30,15 @@ public class IMS_Connect extends JDialog  {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextArea text_1;
+	private JTextPane text_1;
 	private JTextField text_2;
 	private JScrollPane scrollPane1;
 	private JScrollPane scrollPane;
 	private JPanel panel;
 	private JPanel buttonPane;
-	private JTextField textField;
+	private StyledDocument doc;
+	private SimpleAttributeSet left;
+	private SimpleAttributeSet right;
 
 	/**
 	 * Launch the application.
@@ -49,13 +61,28 @@ public class IMS_Connect extends JDialog  {
 		setBounds(100, 100, 450, 443);
 		getContentPane().setLayout(new BorderLayout());
 			
-	
 		{
 			buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+
+						try {
+							doc.setParagraphAttributes(doc.getLength(), 1, right, false);
+							doc.insertString(doc.getLength(), "\nJohn: \n" + text_2.getText() + "\n", right);
+							
+
+						} catch (BadLocationException e1) {
+							e1.printStackTrace();
+						}
+
+						System.out.println(text_2.getText());
+						text_2.setText("");
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
@@ -74,7 +101,18 @@ public class IMS_Connect extends JDialog  {
 				scrollPane = new JScrollPane();
 				panel.add(scrollPane, "cell 0 0 0 6,grow");
 				{
-					text_1 = new JTextArea();
+					text_1 = new JTextPane();
+					text_1.setFont(new Font("Dialog", Font.BOLD, 16));
+
+			        doc = text_1.getStyledDocument();
+
+			        left = new SimpleAttributeSet();
+			        StyleConstants.setAlignment(left, StyleConstants.ALIGN_LEFT);
+			        StyleConstants.setForeground(left, Color.RED);
+
+			        right = new SimpleAttributeSet();
+			        StyleConstants.setAlignment(right, StyleConstants.ALIGN_RIGHT);
+			        StyleConstants.setForeground(right, Color.BLUE);
 					scrollPane.setViewportView(text_1);
 				}
 			}
@@ -99,13 +137,19 @@ public class IMS_Connect extends JDialog  {
             public void actionPerformed(ActionEvent e)
             {
             	
-				text_1.append(text_2.getText() + "\n");
-            	System.out.println(text_2.getText());
-            	text_2.setText("");	               
+				
+            	   try
+                   {
+            		   doc.setParagraphAttributes(doc.getLength(), 1, left, false);
+            		   doc.insertString(doc.getLength(), "\nYou: \n" + text_2.getText() + "\n", left );
+                       text_2.setText("");	
+                   } catch(Exception e1) { System.out.println(e); }
             }
         };
 		 
         text_2.addActionListener( action );
 	}
+	
+	 
 
 }
---
+++ transalteIMS/src/ims_server/ChatServerThread.java
@@ -0,0 +1,94 @@
+package ims_server;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+
+
+public class ChatServerThread extends WebSocketServer {
+
+	public ChatServerThread( int port ) throws UnknownHostException {
+		super( new InetSocketAddress( port ) );
+	}
+
+	public ChatServerThread( InetSocketAddress address ) {
+		super( address );
+	}
+
+	@Override
+	public void onOpen( WebSocket conn, ClientHandshake handshake ) {
+		this.sendToAll( "new connection: " + handshake.getResourceDescriptor() );
+		System.out.println( conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!" );
+	}
+
+	@Override
+	public void onClose( WebSocket conn, int code, String reason, boolean remote ) {
+		this.sendToAll( conn + " has left the room!" );
+		System.out.println( conn + " has left the room!" );
+	}
+
+	@Override
+	public void onMessage( WebSocket conn, String message ) {
+		this.sendToAll( message );
+		System.out.println( conn + ": " + message );
+	}
+	
+//
+//	@Override
+//	public void onFragment( WebSocket conn, Framedata fragment ) {
+//		System.out.println( "received fragment: " + fragment );
+//	}
+
+	public static void main( String[] args ) throws InterruptedException , IOException {
+		WebSocketImpl.DEBUG = true;
+		int port = 8887; // 843 flash policy port
+		try {
+			port = Integer.parseInt( args[ 0 ] );
+		} catch ( Exception ex ) {
+		}
+		ChatServerThread s = new ChatServerThread( port );
+		s.start();
+		System.out.println( "ChatServer started on port: " + s.getPort() );
+
+		BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+		while ( true ) {
+			String in = sysin.readLine();
+			s.sendToAll( in );
+			if( in.equals( "exit" ) ) {
+				s.stop();
+				break;
+			} else if( in.equals( "restart" ) ) {
+				s.stop();
+				s.start();
+				break;
+			}
+		}
+	}
+	@Override
+	public void onError( WebSocket conn, Exception ex ) {
+		ex.printStackTrace();
+		if( conn != null ) {
+			// some errors like port binding failed may not be assignable to a specific websocket
+		}
+	}
+
+
+	public void sendToAll( String text ) {
+		Collection<WebSocket> con = connections();
+		synchronized ( con ) {
+			for( WebSocket c : con ) {
+				c.send( text );
+			}
+		}
+	}
+}
